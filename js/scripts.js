db2377d3f681ed9a36cb2c6028100254d8c19ae4
AlexMelnychuk <melnychuk.alexander@gmail.com>
2022-09-17 22:21:23 +0300

    created header and fixed owl carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        items: 1,
-        nav:true,
-        // dots: false,
-    });
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel({
+//         loop: true,
+//         items: 1,
+//         nav:true,
+//
+//
+//     });
+// });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:false,
+    autoplayTimeout: 1000,
+    dots: true,
+    nav:true,
+    navText: [ '', ' ' ],
+    items:1,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //
+    //     1000:{
+    //         items:1
+    //     }
+    // }
 });
